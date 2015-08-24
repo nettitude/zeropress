d0ace8f6cc8852a204c429cd7c089d28d853e4f1
Strawp <github@strawp.net>
2015-08-24 16:41:25 +0100

    Added ability to parse wpscan output for plugins

--- zeropress.py
+++ zeropress.py
@@ -9,8 +9,12 @@ from bs4 import BeautifulSoup as bs
 # Defaults
 plugindir = "https://wordpress.org/plugins/browse/popular/"
 outputdir = "plugins"
-logfile = "zeropress_"+str( datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") )+".log"
+logfile = "logs/zeropress_"+str( datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") )+".log"
 
+# Base URL of wordpress plugin downloads by version
+downloadbase = 'https://downloads.wordpress.org/plugin/'
+
+# Print an info message
 def pinfo(info):
   print "\033[92m[I] " + info + "\033[0m"
 
@@ -42,15 +46,29 @@ def scrape_plugindir(plugindir):
   pinfo( "All done! Looks like we got all the plugin pages." )
   sys.exit
 
+# Parse wpscan output and get all the mentioned plugins
+def parse_wpscan_output( wpscanfile ):
+  with open(wpscanfile) as f:
+    content = f.read()
+  plugins = re.findall( "Name: ([-a-z0-9]+) - v([\.0-9]+)", content )
+  for plugin in plugins:
+    get_specific_plugin_version(plugin[0],plugin[1])
 
+# Download a zip of the latest version of a plugin of its plugin page
 def get_latest_plugin_version(pluginpage):
   global args
   pinfo( "Getting plugin page: " + pluginpage )
   shortname = re.findall('([^\/]+)\/?$',pluginpage)[0]
   r = requests.get(pluginpage)
   soup = bs( r.text )
-  downloadurl = soup.find_all( 'a', attrs={'itemprop': 'downloadUrl'})[0]['href']
   version = soup.find_all( 'meta', attrs={'itemprop': 'softwareVersion'})[0]['content']
+  get_specific_plugin_version( shortname, version )
+
+# Get a specific plugin version. Get the latest if specific version not available
+def get_specific_plugin_version( shortname, version ):
+  global downloadbase
+  pinfo( "Getting " + shortname + " v" + version )
+  downloadurl = downloadbase + shortname + '.' + version + '.zip'
   path = args.outputdir + '/' + shortname + '/' + version 
   filename = downloadurl.split('/')[-1]
   zippath = path + '/' + filename
@@ -61,20 +79,25 @@ def get_latest_plugin_version(pluginpage):
   if not os.path.exists( zippath ):
     pinfo( "Downloading " + downloadurl + " to " + path )
     r = requests.get(downloadurl)
-    z = open( zippath, 'w' )
-    z.write( r.content )
-    z.close()
-    unpack_zip( zippath )
+    if r.status_code != 200:
+      print "\033[91m[.] Download failed for " + downloadurl + ": " + str( r.status_code ) + "\033[0m"
+    else:
+      z = open( zippath, 'w' )
+      z.write( r.content )
+      z.close()
+      unpack_zip( zippath )
   else:
     print "[.] Zip already present in " + path
   
   analyse_code( path )
 
+# Unpack a zip
 def unpack_zip( zippath ):
   dest = '/'.join(zippath.split('/')[:-1])
   print "[.] Unpacking " + zippath
   subprocess.check_output(['unzip', '-o', '-d', dest, zippath])
 
+# Analyse newest version of all plugins in a plugin dir
 def analyse_all_plugins(plugindir):
   print "[.] Analysing newest version of all plugins currently in " + plugindir
   # List dirs in plugindir
@@ -89,6 +112,7 @@ def analyse_all_plugins(plugindir):
       break
   pinfo( "Current version of all plugins available analysed" )
 
+# Test code in the given dir with a number of easy to spot coding errors
 def analyse_code( codedir ):
  print "[.] Analysing code in " + codedir 
  code_search( 'grep -rHnI "[^\._a-z]\(eval\|passthru\|system\|exec\|shell_exec\|pcntl_exec\|popen\|proc_open\)([^\$]*\$[^\$]*)" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "RCE" )
@@ -99,6 +123,7 @@ def analyse_code( codedir ):
  code_search( 'grep -rHnI "\. *\$_\(GET\|POST\|COOKIE\|REQUEST\)\[" '+codedir+' | grep "\(file_get_contents\|fopen\|SplFileObject\)("', "LFI" )
  code_search( 'grep -rHnI "\. *\$_\(GET\|POST\|COOKIE\|REQUEST\)\[" '+codedir+' | grep "\(<\w\|\w>\)"', "XSS" )
 
+# Search using a given grep command, parse and log the response
 def code_search( cmd, genre="" ):
   global args
   out = ''
@@ -114,18 +139,29 @@ def code_search( cmd, genre="" ):
     print out
   return out
   
+#
+# Start
+#
 
+# Command line options
 parser = argparse.ArgumentParser(description="Grab the most popular wordpress plugins, unpack them and look for dangerous code use")
 parser.add_argument("-d", "--plugindir", help="Base URL for scraping plugins", default=plugindir)
 parser.add_argument("-o", "--outputdir", help="Output dir for saving downloaded files", default=outputdir)
 parser.add_argument("-l", "--logfile", help="Log file to write to", default=logfile)
+parser.add_argument("-w", "--wpscan", help="Download all plugins mentioned in the supplied output file from wpscan", default=logfile)
 parser.add_argument("-n", "--nodownload", action="store_true", help="Don't do any scraping, just analyse any code already present")
 args = parser.parse_args()
 
 pinfo( "Logging to " + args.logfile )
 sys.exit
 
+logdir = os.path.dirname(args.logfile)
+if not os.path.exists( logdir ):
+  os.makedirs( logdir )
+
 if args.nodownload:
   analyse_all_plugins(args.outputdir)
+elif args.wpscan:
+  parse_wpscan_output( args.wpscan )
 else:
   scrape_plugindir( args.plugindir )
