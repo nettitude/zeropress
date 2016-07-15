a1162ba27928412bd59fcdb003d8b5799bb253bf
Strawp <github@strawp.net>
2016-07-15 11:21:50 +0100

    Added severity command line switch

--- zeropress.py
+++ zeropress.py
@@ -161,47 +161,56 @@ def analyse_code( codedir ):
  code_search( 'grep -irHn'+binmode+' "\(mysql_query\|mssql_query\|pg_query\|mysqli_query\|db_query\)" ' + codedir+' | grep "'+uservar+'"', "SQLI" )
  code_search( 'grep -irHn'+binmode+' "db->\(get_row\|get_results\|query\|get_var\)" ' + codedir+' | grep "'+uservar+'"', "SQLI" )
 
- # SSRF
- code_search( 'grep -rHn'+binmode+' "\(curl_exec\|ftp_connect\|ftp_ssl_connect\|pfsockopen\|socket_bind\|socket_connect\|socket_listen\|socket_create_listen\|socket_accept\|socket_getpeername\|socket_send\|curl_init\|fsockopen\|stream_context_create\|get_headers\)(" '+codedir+' | grep "'+uservar+'"', "SSRF" )
- code_search( 'grep -rHn'+binmode+' "CURLOPT_URL" '+codedir+' | grep "'+uservar+'"', "SSRF" )
+ # High severity issues
+ if args.severity >= 2:
  
- # Object injection
- code_search( 'grep -rHn'+binmode+' "'+uservar+'" '+codedir+' | grep "unserialize("', "OBJI" )
+   # Object injection
+   code_search( 'grep -rHn'+binmode+' "'+uservar+'" '+codedir+' | grep "unserialize("', "OBJI" )
+   
+   # File upload handling
+   code_search( 'grep -rHn'+binmode+' "\$_FILES\[[\\"\'][^\\"\']\+[\\"\']\]\[[\\"\']name[\\"\']\]" ' + codedir, "FILE" )
+   
+   # SSRF
+   code_search( 'grep -rHn'+binmode+' "\(curl_exec\|ftp_connect\|ftp_ssl_connect\|pfsockopen\|socket_bind\|socket_connect\|socket_listen\|socket_create_listen\|socket_accept\|socket_getpeername\|socket_send\|curl_init\|fsockopen\|stream_context_create\|get_headers\)(" '+codedir+' | grep "'+uservar+'"', "SSRF" )
+   code_search( 'grep -rHn'+binmode+' "CURLOPT_URL" '+codedir+' | grep "'+uservar+'"', "SSRF" )
+   
+   # Local file inclusion
+   code_search( 'grep -rHn'+binmode+' "\$\w\+" '+codedir+' | grep "\(file_get_contents\|fopen\|SplFileObject\|include\|require\|include_once\|require_once\|show_source\|highlight_file\)("', "LFI" )
+   
+   # XSS
+   code_search( 'grep -rHn'+binmode+' "'+uservar+'" '+codedir+' | grep "\(<\w\|\w>\)"', "XSS" )
  
- # Local file inclusion
- code_search( 'grep -rHn'+binmode+' "\$\w\+" '+codedir+' | grep "\(file_get_contents\|fopen\|SplFileObject\|include\|require\|include_once\|require_once\|show_source\|highlight_file\)("', "LFI" )
- 
- # XSS
- code_search( 'grep -rHn'+binmode+' "'+uservar+'" '+codedir+' | grep "\(<\w\|\w>\)"', "XSS" )
- 
- # Code control
- code_search( 'grep -rHn'+binmode+' "[^\._a-z]\(call_user_func\|call_user_func_array\)([^\$]*\$[^\$]*)" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
- code_search( 'grep -rHn'+binmode+' "\$\w\+(" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
- code_search( 'grep -irHn'+binmode+' "function \+__\(destruct\|wakeup\|tostring\)(" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
- 
- # CRLF Injection
- code_search( 'grep -irHn'+binmode+' "\Wheader(" '+codedir+' | grep "'+uservar+'"', "CRLF" )
+   # CRLF Injection
+   code_search( 'grep -irHn'+binmode+' "\Wheader(" '+codedir+' | grep "'+uservar+'"', "CRLF" )
 
- # phpinfo()
- code_search( 'grep -rHn'+binmode+' "phpinfo(" '+codedir, "INFO" )
-
- # Debug functionality
- code_search( 'grep -rHn'+binmode+' "'+uservar+'[\'\\"]\(test\|debug\)" '+codedir, "DBUG" )
+ # Medium severity issues
+ if args.severity >= 3:
  
- # Ability to declare a variable into the current scope
- code_search( 'grep -irHn'+binmode+' "parse_str( *'+uservarany+' *)" ' + codedir, "VARS" )
+   # Code control
+   code_search( 'grep -rHn'+binmode+' "[^\._a-z]\(call_user_func\|call_user_func_array\)([^\$]*\$[^\$]*)" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
+   code_search( 'grep -rHn'+binmode+' "\$\w\+(" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
+   code_search( 'grep -irHn'+binmode+' "function \+__\(destruct\|wakeup\|tostring\)(" '+codedir+' | grep -v "\.\(js\|css\|js\.php\):"', "CTRL" )
+   
+   # Debug functionality
+   code_search( 'grep -rHn'+binmode+' "'+uservar+'[\'\\"]\(test\|debug\)" '+codedir, "DBUG" )
+   
+   # Ability to declare a variable into the current scope
+   code_search( 'grep -irHn'+binmode+' "parse_str( *'+uservarany+' *)" ' + codedir, "VARS" )
 
- # File upload handling
- code_search( 'grep -rHn'+binmode+' "\$_FILES\[[\\"\'][^\\"\']\+[\\"\']\]\[[\\"\']name[\\"\']\]" ' + codedir, "FILE" )
+   # Weak crypto
+   code_search( 'grep -rHn'+binmode+' "md5(" '+codedir, "CRYP" )
+   code_search( 'grep -rHn'+binmode+' "CRYPT_MD5" '+codedir, "CRYP" )
+   code_search( 'grep -rHn'+binmode+' "CRYPT_EXT_DES" '+codedir, "CRYP" )
+   code_search( 'grep -rHn'+binmode+' "CRYPT_STD_DES" '+codedir, "CRYP" )
 
- # Weak crypto
- code_search( 'grep -rHn'+binmode+' "md5(" '+codedir, "CRYP" )
- code_search( 'grep -rHn'+binmode+' "CRYPT_MD5" '+codedir, "CRYP" )
- code_search( 'grep -rHn'+binmode+' "CRYPT_EXT_DES" '+codedir, "CRYP" )
- code_search( 'grep -rHn'+binmode+' "CRYPT_STD_DES" '+codedir, "CRYP" )
+ # Low severity issues
+ if args.severity == 4:
+   
+   # phpinfo()
+   code_search( 'grep -rHn'+binmode+' "phpinfo(" '+codedir, "INFO" )
 
- # Todo items
- code_search( 'grep -rHni'+binmode+' "\W\(TODO\|FIXME\|HACK\)\W" '+codedir+' | grep "\.php:"', "TODO", True )
+   # Todo items
+   code_search( 'grep -rHni'+binmode+' "\W\(TODO\|FIXME\|HACK\)\W" '+codedir+' | grep "\.php:"', "TODO", True )
 
 # Search using a given grep command, parse and log the response
 def code_search( cmd, genre="", allowcomments=False ):
@@ -241,6 +250,7 @@ parser.add_argument("-w", "--wpscan", help="Download all plugins mentioned in th
 parser.add_argument("-n", "--nodownload", action="store_true", help="Don't do any scraping, just analyse any code already present")
 parser.add_argument("-a", "--analyse", help="Just analyse a folder without doing anything else")
 parser.add_argument("-b", "--binaries", action="store_true", help="Search within binary files as if they were text")
+parser.add_argument("-s", "--severity", choices=['1','2','3','4'], help="Report only issues of this severity level and up (1=critical, 4=medium)")
 parser.add_argument("--debug", help="Output search commands")
 args = parser.parse_args()
 
@@ -252,6 +262,11 @@ else:
 logdir = os.path.dirname(args.logfile)
 if not os.path.exists( logdir ):
   os.makedirs( logdir )
+
+if not args.severity:
+  args.severity = 4
+
+args.severity = int(args.severity)
 
 if args.analyse:
   analyse_code(args.analyse)
